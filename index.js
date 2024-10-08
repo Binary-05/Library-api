53a88124b1542aa2208f66c44cf74fa485cd64df
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2024-10-08 10:21:35 +0000

    js modified

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import { bookRouter } from "./routes/book-routes.js";
 import authorRouter from "./routes/author-routes.js";
+import bookRouter from "./routes/book-routes.js";
+import cors from "cors";
 
 
 // Connect to databsse
@@ -12,10 +13,15 @@ await mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
 
-app.use(bookRouter);
+// Use middlewares
+app.use(cors());
+
+
+app.use(bookRouter)
 app.use(authorRouter);
+
 
 // Listening for income request
 app.listen(7979, () => {
-    console.log("App is listen on port 7979");
+    console.log("App is listening on port 7979");
 }) 
