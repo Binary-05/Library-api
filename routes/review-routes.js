30641fc0ce4199de32b99d00429c5bb0cd630559
jessiakosua <jessica.a.adika@gmail.com>
2024-10-09 11:36:25 +0000

    added validation

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { addReview, deleteReview, getAllReviewsForBook, getReviewForBook, updateReview } from "../controllers/reviews-controllers.js";
+import { reviewValidationSchema } from "../middleware/validate-review.js";
 
 
 
 const reviewRouter = Router();
 
 //add all routes
-reviewRouter.post("/review", addReview);
+reviewRouter.post("/review", reviewValidationSchema, addReview);
 reviewRouter.get("/review/:id", getReviewForBook);
 reviewRouter.get("/reviews",  getAllReviewsForBook);
-reviewRouter.patch("/review/:id", updateReview);
+reviewRouter.patch("/review/:id", reviewValidationSchema, updateReview);
 reviewRouter.delete("/review/:id", deleteReview);
 
 export default reviewRouter
