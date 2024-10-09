ef8ab2b085045a051fa5be0780c0379141e59224
Rodney Hinson <rodneyhinson22@gmail.com>
2024-10-09 11:12:08 +0000

    modified js files

--- routes/author-routes.js
+++ routes/author-routes.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { addAuthor, deleteAuthorInfo, getAllAuthors, getAuthor, updateAuthorInfo } from "../controllers/author-controllers.js";
+import { authorValidationSchema } from "../middleware/validate-author.js";
 
 //
 const authorRouter = Router();
 
 //
-authorRouter.post("/author", addAuthor);
+authorRouter.post("/author", authorValidationSchema, addAuthor);
 
 authorRouter.get("/author/:id", getAuthor);
 
 authorRouter.get("/authors", getAllAuthors);
 
-authorRouter.patch("/author/:id", updateAuthorInfo);
+authorRouter.patch("/author/:id", authorValidationSchema, updateAuthorInfo);
 
 authorRouter.delete("/author/:id", deleteAuthorInfo);
 
